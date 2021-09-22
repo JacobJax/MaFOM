e6f1921280f01676e68abfda2759db1a2d7f09bf
Jacob Odero <jackjax617@gmail.com>
2021-09-22 11:10:38 +0200

    change to config vars

--- db/db_config.php
+++ db/db_config.php
@@ -5,7 +5,13 @@ function establishCONN() {
     // $pdo = new PDO('mysql:host=localhost;port=3306;dbname=mafom_mgt', 'root', '');
 
     // production database
-    $pdo = new PDO('mysql:host=bhrf5uvgouxajlovmlzq-mysql.services.clever-cloud.com;port=3306;dbname=bhrf5uvgouxajlovmlzq', 'uxjctau3wo4krd3g', 'fgDA5OIeL6wDaBXIZvVp');
+    $db_host = getenv('CC_DB_HOST');
+    $db_port = getenv('CC_DB_PORT');
+    $db_name = getenv('CC_DB_NAME');
+    $db_user = getenv('CC_DB_USER');
+    $db_pwd = getenv('CC_DB_PWD');
+
+    $pdo = new PDO("mysql:host=$db_host;port=$db_port;dbname=$db_name", "$db_user" , "$db_pwd");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     return $pdo;
